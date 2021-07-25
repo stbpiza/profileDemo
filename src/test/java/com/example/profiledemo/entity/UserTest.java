57c4f683f5a97490a2c827d12276dd9530f90f10
stbpiza <stbpiza@gmail.com>
2021-07-25 23:34:37 +0900

    테스트 환경 나누기

---
+++ src/test/java/com/example/profiledemo/entity/UserTest.java
@@ -0,0 +1,16 @@
+package com.example.profiledemo.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserTest {
+
+    @Test
+    void simpleTest() {
+
+    }
+
+}
